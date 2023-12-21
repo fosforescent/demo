272c83d9af3ebd14e94cbdf150fdf3b2e3339317
davidmnoll <davidmnoll@users.noreply.github.com>
2023-12-21 13:34:44 -0500

    updated hamburger menu

---
+++ src/assets.ts
@@ -0,0 +1,4 @@
+export const images = {
+  ethCode : new URL('../public/eth.png', import.meta.url).href,
+  btcCode : new URL('../public/btc.png', import.meta.url).href,
+}
--- src/components/menu/HamburgerMenu.tsx
+++ src/components/menu/HamburgerMenu.tsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 
 import { useAppState } from '@/components/app-state'
- 
+import { buttonVariants } from "@/components/ui/button"
+
+import { images } from '@/assets'
+
 import {
   Sheet,
   SheetContent,
@@ -12,13 +15,14 @@ import {
   SheetClose
 } from "@/components/ui/sheet"
 import { Input } from '../ui/input'
+import { Button } from '../ui/button'
 
 
 
 const HamburgerMenu = () => {
   const [menuOpen, setMenuOpen] = useState(false)
 
-
+  const [infoOpen, setInfoOpen] = useState(false)
 
   const [token, setToken] = React.useState(localStorage.getItem("token") || "");
 
@@ -38,9 +42,32 @@ const HamburgerMenu = () => {
       <SheetTitle>
         Settings
       </SheetTitle>
-      <div className='padding-lg'>
-        <Input placeholder="openai token" value={token} onChange={updateToken} />
+      <div className='p-3'>
+        <label className='block text-sm font-medium text-gray-700' htmlFor="token">
+          OpenAI API Token - warning: this is stored in your browser's localStorage 
+        </label>
+        <Input name="token" placeholder="openai token" value={token} onChange={updateToken} />
+      </div>    
+      <div className='p-3'>
+        <Button variant='secondary' onClick={() => setInfoOpen(!infoOpen)}>Why? {infoOpen? '-' : '+'}</Button><br />
+        {infoOpen ? 
+          (<><a href='/' target='_blank' className='underline text-slate-400' >Fosforescent</a>&nbsp;
+          is trying to become a visual programming language that intertwines human, computer,
+          and AI instructions providing you with an interface to make your next steps clear, automate away your tedious tasks, and allow
+          efficient decentralized planning and coordination
+          </>)
+          : <></>}
       </div>
+      <div className='p-3'>
+        <a href='https://buy.stripe.com/8wMdSz0rT6AKejKcMM' target='_blank' className={`${buttonVariants({ variant: "ghost" })} bg-emerald-900 `}>Tip $1 or more</a>
+      </div>
+      <div className='p-3 h-32 w-32'>
+        <img src={images.btcCode} alt="btc-qrcode" className='object-stretch shrink-1' />
+      </div>
+      <div className='p-3 h-32 w-32'>
+        <img src={images.ethCode} alt="eth-qrcode" className='object-stretch shrink-1' />
+      </div>
+
     </SheetContent>
   </Sheet> 
   </div>
